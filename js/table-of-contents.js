d60275824633cf0b594ba56d3afbe09fdedb722b
hpa <PavelQuash@users.noreply.github.com>
2022-09-23 12:31:46 +0500

    Сделал в постах Table of Contents по заголовкам h2

---
+++ js/table-of-contents.js
@@ -0,0 +1,28 @@
+/* Формирование оглавления страницы по заголовкам */
+/* Для формирования на странице необходим элемент с id='table-of-contents' */
+
+function generateTableOfContents() {
+	var toc = document.getElementById("table-of-contents");
+	if (toc == null) return;
+	
+    tocList = document.createElement("ul");    
+    headers = document.getElementsByTagName("h2");
+   
+    for (i = 0; i < headers.length; i++){
+	  var name = "h"+i;
+	  if (headers[i].id == null) { headers[i].id = name; } 
+	  else { name = headers[i].id; }
+     
+      // a list item for the entry
+      tocListItem = document.createElement("li");
+
+      // a link for the h3
+      tocEntry = document.createElement("a");
+      tocEntry.setAttribute("href","#"+name);
+      tocEntry.innerText=headers[i].innerText;
+     
+      tocListItem.appendChild(tocEntry);
+      tocList.appendChild(tocListItem);
+    }
+    toc.appendChild(tocList);
+}
